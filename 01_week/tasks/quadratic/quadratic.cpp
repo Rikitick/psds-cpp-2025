ee50f0981889270d92d6b37578e6a248835fd8f9
Rikitick <rick.tick15@mail.ru>
2025-11-27 00:52:36 +0500

    add (solution): add quadratic task

--- 01_week/tasks/quadratic/quadratic.cpp
+++ 01_week/tasks/quadratic/quadratic.cpp
@@ -1,6 +1,49 @@
+#include <cmath>
 #include <stdexcept>
+#include <iomanip>
+#include <bits/stdc++.h>
 
 
 void SolveQuadratic(int a, int b, int c) {
-    throw std::runtime_error{"Not implemented"};
+    if (a == 0 && b == 0 && c == 0)
+        std::cout << "infinite solutions";
+    else if (a == 0 && b == 0 && c != 0)
+        std::cout << "no solutions";
+    else if (a == 0 && b != 0 && c == 0)
+        std::cout << "0";
+    else if (a == 0 && b != 0 && c != 0)
+        std::cout << std::setprecision(6) << (long double)-c / (long double)b;
+    else if (a != 0 && b == 0 && c == 0)
+        std::cout << "0";
+    else if (a != 0 && b == 0 && c != 0) {
+        if (c > 0)
+            std::cout << "no solutions";
+        else {
+            long double x = sqrt((long double)-c / (long double)a);
+            std::cout << std::setprecision(6) << -x << ' ' << x;
+        }   
+    }
+    else if (a != 0 && b != 0 && c == 0) {
+        long double x = (long double)-b / (long double)a;
+        if (x < 0)
+            std::cout << std::setprecision(6) << x << " 0";
+        else
+            std::cout << std::setprecision(6) << "0 " << x;
+    }
+    else {
+        long long d = b*b - 4*a*c;
+        if (d < 0)
+            std::cout << "no solutions";
+        else if (d == 0)
+            std::cout << std::setprecision(6) << (long double)-b / (2.0L*a);
+        else {
+            long double sqrtd = sqrt(d);
+            long double x1 = ((long double)-b + sqrtd) / (2.0L*a);
+            long double x2 = ((long double)-b - sqrtd) / (2.0L*a);
+            if (x1 < x2)
+                std::cout << std::setprecision(6) << x1 << ' ' << x2;
+            else
+                std::cout << std::setprecision(6) << x2 << ' ' << x1;
+        }
+    }
 }
