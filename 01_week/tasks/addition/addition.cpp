5aa6519f885b57ce03c0e97d306ba37c49b86f14
Rikitick <rick.tick15@mail.ru>
2025-12-04 19:52:58 +0500

    add (solution): add addition task

--- 01_week/tasks/addition/addition.cpp
+++ 01_week/tasks/addition/addition.cpp
@@ -3,5 +3,7 @@
 
 
 int64_t Addition(int a, int b) {
-    return (int64_t)a + (int64_t)b;
+    int64_t first = static_cast<int64_t>(a);
+    int64_t second = static_cast<int64_t>(b);
+    return first + second;
 }
