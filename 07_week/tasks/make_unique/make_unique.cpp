78bfc55eba4fdcc0f6e483a9b695ccfabc2408fa
Rikitick <rick.tick15@mail.ru>
2026-03-09 22:29:26 +0500

    add (solution): add make_unique task

--- 07_week/tasks/make_unique/make_unique.cpp
+++ 07_week/tasks/make_unique/make_unique.cpp
@@ -1,4 +1,8 @@
 #include <memory>
+#include <utility>
 
-
-/* return_type */ MakeUnique( /* args */ );
+template<typename T, typename... Args>
+std::unique_ptr<T> MakeUnique(Args&&... args) {
+    std::unique_ptr<T> ptr = std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    return ptr;
+}
